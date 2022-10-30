c6abd35bdbd3fc1f723a3b48fd66d19f8c24e765
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2022-10-30 15:05:12 +0800

    add final funcctionality for post requests

---
+++ Develop/ROUTES/api.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+const uuid = require('uuid');
+
+const dbPath = path.join(__dirname, '../db/db.json');
+
+/**
+ * 
+ * @returns {Array}
+ */
+
+// Helper function which will convert notes to json format and ensure that if no notes are added, an empty array is returned instead of an empty string
+function getNotes(){
+
+    // read db json
+    const json = fs.readFileSync(dbPath, 'utf-8');
+    //json parse
+    try{
+        return JSON.parse(json);
+    }catch(err){
+        return [];
+    }
+}
+
+router.get('/api/notes', (req, res) => {
+    
+    //retrieve the notes from the db.json
+    res.json(getNotes())
+});
+
+router.post('/api/notes', (req, res) => {
+
+    // create new note
+    const newNotes = {
+        id: uuid.v4(),
+        text: req.body.text,
+        title: req.body.title,
+    }
+    // add to db json
+    const existing = getNotes();
+
+    existing.push(newNotes);
+
+    fs.writeFileSync(dbPath, JSON.stringify(existing), 'utf-8');
+
+    res.json(newNotes);
+})
+
+// it is then exported to be used in other files such as app.js
+module.exports = router;
--- Develop/app.js
+++ Develop/app.js
@@ -2,8 +2,9 @@
 // Requiring express to gain access to the express methods
 const express = require('express');
 
-// gain access to the web js file inside the router
+// gain access to the web js and api jsfile inside the router
 const webRouter = require('./routes/web')
+const apiRouter = require('./routes/api');
 
 // putting the express method into a variable.
 const app = express();
@@ -11,9 +12,14 @@ const app = express();
 // defining the port number for the app to listen into. process.env allows for heroku.
 const PORT = process.env.PORT || 6100;
 
+// Middleware that allows use of assets files such as CSS and JS files
+app.use(express.static(__dirname + '/public' ));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 // use method puts the middleware function to the specified path.
 app.use(webRouter);
+app.use(apiRouter);
 
 // listen function binds and listens to connections on the specified host and port.
 app.listen(PORT, () => {
